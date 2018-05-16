5c549e006bcf1326b304489809ab45a13ae1a6f2
unknown <baptist@bbh-mis-12.bbh.com>
2018-05-16 09:10:42 +0530

    downloads will be stored in root directory

--- rootdir.bat
+++ rootdir.bat
@@ -4,20 +4,18 @@ set mypath=%cd%
 @echo %mypath%
 
 
-MKDIR Dev 
-
-MKDIR Dev\Downloads
+MKDIR %CD:~0,3%\Dev\Downloads
 
 pause
-echo "Dev\Downloads Created.........."
+echo "%CD:~0,3%\Dev\Downloads Created.........."
 
 
 pause
 echo "Installing VSCode.........."
 
-CALL powershell -Command "(New-Object Net.WebClient).DownloadFile('https://vscode-update.azurewebsites.net/latest/win32-x64/stable', 'Dev\Downloads\VScodeSetup.exe')"
+CALL powershell -Command "(New-Object Net.WebClient).DownloadFile('https://vscode-update.azurewebsites.net/latest/win32-x64/stable', '%CD:~0,3%\Dev\Downloads\VScodeSetup.exe')"
 	
-start Dev\Downloads\VSCodeSetup.exe /VERYSILENT /MERGETASKS=!runcode
+start %CD:~0,3%\Dev\Downloads\VSCodeSetup.exe /VERYSILENT /MERGETASKS=!runcode
 
 echo "VSCode installed.........."
 
@@ -26,9 +24,9 @@ pause
 
 echo "Installing nodejs.........."
 
-CALL powershell -Command "(New-Object Net.WebClient).DownloadFile('https://nodejs.org/dist/v10.1.0/node-v10.1.0-x64.msi', 'Dev\Downloads\nodejs.msi')"
+CALL powershell -Command "(New-Object Net.WebClient).DownloadFile('https://nodejs.org/dist/v10.1.0/node-v10.1.0-x64.msi', '%CD:~0,3%\Dev\Downloads\nodejs.msi')"
 	
-MSIEXEC.exe /i Dev\Downloads\nodejs.msi ACCEPT=YES /quiet
+MSIEXEC.exe /i %CD:~0,3%\Dev\Downloads\nodejs.msi ACCEPT=YES /quiet
 
 echo "nodejs installed.........."
 
@@ -47,7 +45,7 @@ rem CALL powershell -Command "(New-Object Net.WebClient).DownloadFile('https://g
 rem echo "git downloaded....."
 		  
 
-rem start Dev\Downloads\git.exe /VERYSILENT /MERGETASKS=!runcode
+rem start %CD:~0,3%\Dev1\Downloads\git.exe /VERYSILENT /MERGETASKS=!runcode
 
 
 pause
