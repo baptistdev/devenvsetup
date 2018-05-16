2da98a14bf208b87a3069acfe69b377a1b176052
unknown <baptist@bbh-mis-12.bbh.com>
2018-05-16 08:44:01 +0530

    trying to run git from root directory

--- rootdir.bat
+++ rootdir.bat
@@ -4,7 +4,7 @@ set mypath=%cd%
 @echo %mypath%
 
 
-MKDIR Dev
+MKDIR Dev 
 
 MKDIR Dev\Downloads
 
@@ -37,7 +37,7 @@ pause
 echo "Installing git.........."
 REM -- TODO --
 REM This is failing we are not able to download from git site.Ideally a git build solution or curl the url may workout
-start E:\sample\Git-2.13.1.2-64-bit.exe /VERYSILENT /MERGETASKS=!runcode
+start Git-2.13.1.2-64-bit.exe /VERYSILENT /MERGETASKS=!runcode
 
 echo "git installed.........."
 
